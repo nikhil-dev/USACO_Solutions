a8469f8d7b9f3a51dcda48e80f644dd75dc4d19e
Nikhil Dev Deshmudre <nikhil.dev91@gmail.com>
2012-12-17 02:39:50 -0500

    Greedy Gift Givers Solution (Problem 1.2)

--- usacoCode/src/gift1.java
+++ usacoCode/src/gift1.java
@@ -3,7 +3,7 @@ ID: nikhil_3
 LANG: JAVA
 TASK: gift1
 FILE: gift1.java
-*/
+ */
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -14,63 +14,67 @@ import java.util.LinkedHashMap;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-
 public class gift1 {
-	public static void main (String [] args) throws Exception{
-		
+	public static void main(String[] args) throws Exception {
+
 		// setup an input stream
-		BufferedReader in = new BufferedReader (new FileReader ("gift1.in")); 
-		
+		BufferedReader in = new BufferedReader(new FileReader("gift1.in"));
+
 		// make a table
 		int nPeople = Integer.parseInt(in.readLine());
-		// the value stored in the map keeps track of the total given by each person (which is the result we ultimately want)
-		LinkedHashMap<String, Integer> table = new LinkedHashMap<String, Integer> (nPeople);
-			// A linked hashmap has been used because the problem requires us to print the values in the order they were added. A linked hashmap keeps track of the order teh eements were added in.
+		// the value stored in the map keeps track of the total given by each
+		// person (which is the result we ultimately want)
+		LinkedHashMap<String, Integer> table = new LinkedHashMap<String, Integer>(
+				nPeople);
+		// A linked hashmap has been used because the problem requires us to
+		// print the values in the order they were added. A linked hashmap keeps
+		// track of the order teh eements were added in.
+
 		
 		// populate it
-		for (int i = 0; i < nPeople; i++){
+		for (int i = 0; i < nPeople; i++) {
 			String current = in.readLine();
 			table.put(current, 0);
 		}
-		
+
 		StringTokenizer tokenizer;
 		// do updates for each person
-		for (int i = 0; i < nPeople; i++){
+		for (int i = 0; i < nPeople; i++) {
 			String currentGiver = in.readLine();
 			tokenizer = new StringTokenizer(in.readLine());
-			
+
 			// the initial amount the currentGiver has
 			int initialAmount = Integer.parseInt(tokenizer.nextToken());
-			
+
 			// the number of people currentGiver gives to
 			int nGifts = Integer.parseInt(tokenizer.nextToken());
-						
+
 			if (nGifts == 0)
 				continue;
-			int eachGift = initialAmount/nGifts;
-			
+			int eachGift = initialAmount / nGifts;
+
 			// update the net amount that the current guy has given uptil now.
 			int totalGiven = table.get(currentGiver);
-			totalGiven += eachGift*nGifts;
+			totalGiven += eachGift * nGifts;
 			table.put(currentGiver, totalGiven);
-			
+
 			// update the amounts of the people who receive from currentGiver
-			for (int j = 0; j < nGifts; j++){
+			for (int j = 0; j < nGifts; j++) {
 				String beneficiary = in.readLine();
 				int beneficiaryGiven = table.get(beneficiary);
 				beneficiaryGiven -= eachGift;
 				table.put(beneficiary, beneficiaryGiven);
 			}
 		}
-		
+
 		// print each person along with total amount given to a file
-		PrintWriter out = new PrintWriter (new BufferedWriter(new FileWriter("gift1.out")));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+				"gift1.out")));
 		Set<String> keySet = table.keySet();
-		for (String person : keySet){
-			out.println(person + " " + -1*table.get(person));
+		for (String person : keySet) {
+			out.println(person + " " + -1 * table.get(person));
 		}
-		
-		
+
 		// close the input and output streams
 		out.close();
 		in.close();
