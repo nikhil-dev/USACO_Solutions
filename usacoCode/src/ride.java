119a56b47e34742fcc7e257ce3e4fe5ae0aee5c4
Nikhil Dev Deshmudre <nikhil.dev91@gmail.com>
2012-12-16 21:54:57 -0500

    Initial commit along with first program

---
+++ usacoCode/src/ride.java
@@ -0,0 +1,60 @@
+/*
+ID: nikhil_3
+LANG: JAVA
+TASK: ride
+FILE: ride.java
+*/
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class ride {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+		
+		// setup input stream
+		BufferedReader in = new BufferedReader(new FileReader("ride.in"));
+		
+		// read in the names of the comet and the group
+		String comet, group;
+		comet = in.readLine();
+		group = in.readLine();
+		
+		// determine the result
+		String result = willPickUp(comet, group) ? "GO" : "STAY";
+		
+		// print the result
+		PrintWriter out = new PrintWriter (new BufferedWriter(new FileWriter("ride.out")));
+		out.println(result);
+		
+		// close the files in use		
+		out.close();
+		in.close();
+		
+	}
+	
+	private static boolean willPickUp (String comet, String group){
+		int cometProduct = 1, groupProduct = 1;
+		
+		// find product for comet
+		for (int i = 0; i < comet.length(); i++){
+			cometProduct *= ((int)(comet.charAt(i) - 'A') + 1) % 47;
+		}
+		
+		// find product for group
+		for (int i = 0; i < group.length(); i++){
+			groupProduct *= ((int)(group.charAt(i) - 'A') + 1) % 47;
+		}
+		
+		return (groupProduct == cometProduct);
+	}
+
+}
